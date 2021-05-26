a289b5b33dd64aa0db3b11c328cb267fdad035cd
julio castillo <48731575+ngxk@users.noreply.github.com>
2021-05-25 23:57:05 -0500

    new push

--- hello.go
+++ hello.go
@@ -20,4 +20,33 @@ func main() {
 
 	fmt.Println(uno, dos, tres, val, val2, val3)
 
+	var x bool = true
+
+	if x {
+		fmt.Println("boolenao papu")
+	}
+
+	if 7%2 == 0 {
+		fmt.Printf("Even")
+	} else {
+		fmt.Print("Odd")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	y := 2
+
+	switch y {
+	case 1:
+		fmt.Print("uno")
+	default:
+		fmt.Print("degaulr")
+	}
+
 }
